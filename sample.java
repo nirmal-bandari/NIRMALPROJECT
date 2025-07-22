2338f0b1e220c4f8fc1038d60f490c689a26ed36
root <root@ip-172-31-28-193.eu-north-1.compute.internal>
2025-07-22 09:35:56 +0000

    modified the i value to 70

--- sample.java
+++ sample.java
@@ -1,6 +1,6 @@
 public class PrintNumbers {
     public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++) {
+        for (int i = 1; i <=70; i++) {
             System.out.println(i);
         }
     }
